f27d853b0c7622d3e9c72e64fab3f79e65816600
sebas061199 <s.broekhof@hotmail.com>
2024-04-03 16:10:42 +0200

    link test

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-function openInGoogleMaps(address) {
-    var googleMapsURL = "https://www.google.com/maps?q=" + encodeURI(address);
-    document.getElementById("googleMapsLink").href = googleMapsURL;
+function openInGoogleMaps(event, name, address) {
+    event.preventDefault();
+    var googleMapsURL = "https://www.google.com/maps?q=" + encodeURIComponent(name+ ", " + address);
+    window.open(googleMapsURL, "_blank");
 }
 
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
-        var firstObjectData = data.data[0].address;
-        openInGoogleMaps(firstObjectData);
+        var firstObjectData = data.locaties[0];
+        var googleMapsLink = document.getElementById("googleMapsLink");
+        googleMapsLink.addEventListener("click", function(event) {
+            openInGoogleMaps(firstObjectData.name, firstObjectData.address);
+        });
+        
     })
     .catch(error => console.error('Error fetching JSON: ', error));
